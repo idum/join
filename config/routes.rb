3b19035d1821776be64c1b52489d914815abf92a
Ido Traini <ido.traini@gmail.com>
2024-10-16 12:29:48 +0200

    auth primi passi

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
+  get "home/index"
+  root to: "home#index"
+  get "logout", to: "sessions#destroy"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
