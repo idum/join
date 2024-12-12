fe5a004ec52b420f174510a674ffe045edd0b817
Ido Traini <ido.traini@gmail.com>
2024-12-12 10:29:33 +0000

    inserito schema: clicca e si apre il dettaglio

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   allow_unauthenticated_access
   def index
-    !authenticated? ? (redirect_to welcome_path) : (redirect_to events_path)
+    # !authenticated? ? (redirect_to welcome_path) : (redirect_to events_path)
+    (redirect_to welcome_path) if !authenticated?
   end
 
   def welcome
